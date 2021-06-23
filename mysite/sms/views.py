0f0aed3a40714037a6c9f8bb0eef09d6d9bf043f
Nick Linthacum <nlinthacum@zagmail.gonzaga.edu>
2021-06-23 10:42:47 -0700

    succesfully responds with temp

--- mysite/sms/views.py
+++ mysite/sms/views.py
@@ -2,17 +2,49 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
 from twilio.twiml.messaging_response import MessagingResponse
+import os
+import glob
+import time
+from twilio.rest import Client
 
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+ 
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
+
+def read_temp_raw():
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+ 
+def read_temp():
+    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        temp_f = temp_c * 9.0 / 5.0 + 32.0
+        return temp_f
 
 @csrf_exempt
 def sms_response(request):
     # Start our TwiML response
     resp = MessagingResponse()
+    
+    temp_data = read_temp()
+    print(temp_data)  
+    str(temp_data)
+    my_message = "The temp is: %s" %(temp_data)
 
     # Add a text message
-    msg = resp.message("Check out this sweet owl!")
+    msg = resp.message(my_message)
 
-    # Add a picture message
-    msg.media("https://demo.twilio.com/owl.png")
+    
 
     return HttpResponse(str(resp))
