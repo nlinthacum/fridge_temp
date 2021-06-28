18eb9c6cb541fe968395b7cf7eb2dc5427cc9f11
Nick Linthacum <nlinthacum@zagmail.gonzaga.edu>
2021-06-28 12:51:11 -0700

    sends if temps exceeds range

--- main_sensor.py
+++ main_sensor.py
@@ -9,6 +9,26 @@ os.system('modprobe w1-therm')
 base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
+
+def send_message(message):
+    # Find your Account SID and Auth Token at twilio.com/console
+    # and set the environment variables. See http://twil.io/secure
+    account_sid = os.environ['TWILIO_ACCOUNT_SID']
+    auth_token = os.environ['TWILIO_AUTH_TOKEN']
+    client = Client(account_sid, auth_token)
+
+     
+    my_message = message
+    message = client.messages \
+                    .create(
+                         body= my_message,
+                         from_='+18064294286',
+                         to='+14089814670'
+                     )
+
+    print(message.sid)
+
+    
  
 def read_temp_raw():
     f = open(device_file, 'r')
@@ -28,30 +48,31 @@ def read_temp():
         temp_f = temp_c * 9.0 / 5.0 + 32.0
         return temp_f
     
-temp_data = read_temp()
-#while True:
-print(temp_data)  
-#time.sleep(10)
+def alert_temp(temp):
+    
+    if temp >= 70:
+        str(temp)
+        message = "*ALERT* Temp is %s" %(temp)
+        send_message(message)
+    elif temp <=45:
+        str(temp)
+        message = "*ALERT* Temp is %s" %(temp)
+        send_message(message)
+        
+        
+def main():
+    while True:
+        temp_data = read_temp()
+        print(temp_data)
+        alert_temp(temp_data)
+        
+        time.sleep(1800) #delay 30 min
     
 # Download the helper library from https://www.twilio.com/docs/python/install
 
-
-
-# Find your Account SID and Auth Token at twilio.com/console
-# and set the environment variables. See http://twil.io/secure
-account_sid = os.environ['TWILIO_ACCOUNT_SID']
-auth_token = os.environ['TWILIO_AUTH_TOKEN']
-client = Client(account_sid, auth_token)
-
-str(temp_data)
-my_message = "The temp is: %s" %(temp_data)
-message = client.messages \
-                .create(
-                     body= my_message,
-                     from_='+18064294286',
-                     to='+14089814670'
-                 )
-
-print(message.sid)
+if __name__== "__main__":
+    main()
+   
+    
 
 
